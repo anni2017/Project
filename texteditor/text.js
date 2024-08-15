c1825e28600db929738b2372ef0434e35b94fcbc
Anjali chopariya <134685296+anni2017@users.noreply.github.com>
2024-08-15 23:16:41 +0530

    Delete texteditor/text.js

--- texteditor/text.js
+++
@@ -1,9 +0,0 @@
-let inp = document.querySelector("input");
-
-let p = document.querySelector("p");
-
-inp.addEventListener("input" ,function()
-{
-    p.innerText = inp.value;
-    console.log(inp.value);
-})
